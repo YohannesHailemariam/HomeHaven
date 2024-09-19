6ad4e036e190617b77431b9fc9349b4800633a05
iamjohnhm <jhm90822@gmail.com>
2024-09-19 13:07:35 +0300

    Fixed some issues with the page routing and filter

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,13 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<HomePage />}></Route>
-      <Route path='/' element={<HomePage />}></Route>
+      <Route path='/advert' element={<Advert />}></Route>
+      <Route path='/faq' element={<FAQ />}></Route>
+      <Route path='/notifications' element={<Notifications />}></Route>
+      <Route path='/sell' element={<Sell />}></Route>
+      <Route path='/subscription' element={<Subscription />}></Route>
+      <Route path='/wishlist' element={<Wishlist />}></Route>
+      <Route path='/details' element={<Details />}></Route>
     </Routes>
   )
 }
